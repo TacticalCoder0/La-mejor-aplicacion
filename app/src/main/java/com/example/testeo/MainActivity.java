4ee139a571c8a52116ed5834ed16b9740337bfb5
TacticalCoder0 <urizarcristopherprogramacionb@gmail.com>
2024-09-20 10:10:08 -0600

    Nuevo Metodo Ultra Profesional

--- app/src/main/java/com/example/testeo/MainActivity.java
+++ app/src/main/java/com/example/testeo/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MetodoSuperUltraCool();
     }
+    public void MetodoSuperUltraCool() {
+        System.out.println("HOLA QUE TAL SOY COLOSAL");
+    }
+
 }
